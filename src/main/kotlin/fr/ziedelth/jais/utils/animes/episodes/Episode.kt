14fc92a338331d2390b36da666f05f05a1e342cd
Ziedelth <watteaupierre@gmail.com>
2021-11-14 15:27:29 +0100

    Refactor

--- src/main/kotlin/fr/ziedelth/jais/utils/animes/episodes/Episode.kt
+++ src/main/kotlin/fr/ziedelth/jais/utils/animes/episodes/Episode.kt
@@ -8,9 +8,11 @@ import fr.ziedelth.jais.Jais
 import fr.ziedelth.jais.utils.FileImpl
 import fr.ziedelth.jais.utils.ISO8601
 import fr.ziedelth.jais.utils.Impl
+import fr.ziedelth.jais.utils.animes.countries.CountryImpl
 import fr.ziedelth.jais.utils.animes.episodes.datas.AnimeGenre
 import fr.ziedelth.jais.utils.animes.episodes.datas.EpisodeType
 import fr.ziedelth.jais.utils.animes.episodes.datas.LangType
+import fr.ziedelth.jais.utils.animes.platforms.PlatformImpl
 import java.awt.image.BufferedImage
 import java.net.URL
 import javax.imageio.ImageIO
@@ -35,6 +37,12 @@ data class Episode(
     val duration: Long
 ) {
     @Transient
+    var platformImpl: PlatformImpl? = null
+
+    @Transient
+    var countryImpl: CountryImpl? = null
+
+    @Transient
     var animeBufferedImage: BufferedImage? = null
 
     @Transient
@@ -49,6 +57,8 @@ data class Episode(
             this.platform.uppercase().substring(0 until 4)
         }-${this.eId}-${this.langType.getData(countryInformation?.country?.javaClass)?.data}"
 
+        this.platformImpl = Jais.getPlatformInformation(this.platform)
+        this.countryImpl = Jais.getCountryInformation(this.country)
         Impl.tryCatch { this.animeBufferedImage = FileImpl.resizeImage(ImageIO.read(URL(this.animeImage)), 350, 500) }
         Impl.tryCatch { this.episodeBufferedImage = FileImpl.resizeImage(ImageIO.read(URL(this.image)), 640, 360) }
     }
