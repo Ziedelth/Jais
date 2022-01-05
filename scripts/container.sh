b91d989b01405eafa76b15d3e6b9d961300744d1
Ziedelth <watteaupierre@gmail.com>
2022-01-05 11:41:36 +0100

    Update container.sh

--- scripts/container.sh
+++ scripts/container.sh
@@ -3,6 +3,8 @@
 # Variables
 folder=container
 commands="/bin/bash /bin/ls /bin/touch /bin/cat /bin/mkdir /bin/hostname /bin/sleep /bin/clear"
+cpu_limit=10
+memory_limit=10485760
 
 # Création des dossiers
 mkdir $folder
@@ -28,21 +30,21 @@ for command in $commands ; do
 done
 
 # Montages
-#mount --bind /tmp $folder/tmp
-#mount --bind /media $folder/media
+mount --bind /tmp $folder/tmp
+mount --bind /media $folder/media
 
 # Création d'un sous-script
 echo '#!/bin/bash
 mkdir /sys/fs/cgroup/cpu/conteneur
 mkdir /sys/fs/cgroup/memory/conteneur
 echo "$$" > /sys/fs/cgroup/cpu/conteneur/cgroup.procs
-#echo $$ > /sys/fs/cgroup/memory/conteneur/cgroup.procs
-#echo 1 > /sys/fs/cgroup/cpu/conteneur/cpu.shares
-#echo 10240 > /sys/fs/cgroup/memory/conteneur/memory.limit_in_bytes
+echo $$ > /sys/fs/cgroup/memory/conteneur/cgroup.procs
+echo $2 > /sys/fs/cgroup/cpu/conteneur/cpu.shares
+echo $3 > /sys/fs/cgroup/memory/conteneur/memory.limit_in_bytes
 hostname conteneur
 chroot $1 bash' > subscript
 
 chmod +111 subscript
 
 # Unshare
-unshare --fork --uts --pid --net -- ./subscript $folder
+unshare --fork --uts --pid --net -- ./subscript $folder $cpu_limit $memory_limit
